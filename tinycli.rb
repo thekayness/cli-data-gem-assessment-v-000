c3b5ed42106c9b31184e04b7f6971ddcb97d7210
Kathleen DaSilva <kayjsmyth@gmail.com>
2016-08-10 18:48:08 +0000

    got getlocationquery running finally

--- tinycli.rb
+++ tinycli.rb
@@ -8,14 +8,24 @@ class CommandLineInterface
   BASE_PATH = "http://beermapping.com/webservice/"
   KEY = "21110efff66df69d91ec3909c0a38eed"
 
+  def get_location_query
+    puts "Please enter the initials of a state you would like to search in:"
+    begin
+      puts "A valid state abbreviation is two letters."
+      state = gets.chomp
+    end until state.match(/^(?:(A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|P[AR]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY]))$/)
+#hey we can match states why not
+    puts "Now enter a city:"
+    city = gets.chomp.downcase
+    city_state_formatted = '/' + city + ',' + state.downcase
+  end
 #what do we want to happen?
   def run
     #first welcome user
     puts "\e[H\e[2J"
     puts <<-DOC
-                      o©ºº©oo©oº°©
-                      /           \
-                      |___________|____
+                      o©ºº©oo©oº°º©o
+                      | ___________|____
                       |            |____)
                       |  WELCOME   |  | |
                       |            |  | |
@@ -28,8 +38,10 @@ class CommandLineInterface
                      (______________)
     DOC
     #first ask user for location query
-  #get_location_query
+  get_location_query
   end
+
+
 
 end
 
